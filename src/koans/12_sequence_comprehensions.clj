a2ca1560787fe9d5c3554acf43105e282d81b629
fracisco.mendes <francisco.mendes.ext@synchro.com.br>
2023-02-08 11:04:05 -0300

    roslution of exercises up to k.11

--- src/koans/12_sequence_comprehensions.clj
+++ src/koans/12_sequence_comprehensions.clj
@@ -3,7 +3,7 @@
 
 (meditations
   "Sequence comprehensions can bind each element in turn to a symbol"
-  (= __
+  (= [0 1 2 3 4 5]
      (for [x (range 6)]
        x))
 
@@ -12,12 +12,12 @@
      (map (fn [x] (* x x))
           (range 6))
      (for [x (range 6)]
-       __))
+       (* x x)))
 
   "And also filtering"
   (= '(1 3 5 7 9)
      (filter odd? (range 10))
-     (for [x __ :when (odd? x)]
+     (for [x (range 10) :when (odd? x)]
        x))
 
   "Combinations of these transformations are trivial"
